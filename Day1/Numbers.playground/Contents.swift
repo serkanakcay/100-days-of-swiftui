5866a66d39cff0c0d4d2ce92b747cf9641a6781e
Serkan Akçay <serkanakcayyy@gmail.com>
2024-09-04 14:13:23 +0300

    Day1

---
+++ Day1/Numbers.playground/Contents.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+// MARK: Numbers
+
+let score = 10
+let reallyBig = 1_00_000_000
+
+let lowerScore = score - 2
+let higherScore = score  + 10
+let doubleScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+
+var counter = 10
+counter += 5
+
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+
+
+
+
+// Decimal Numbers
+
+let num = 0.1 + 0.1
+print(num)
+
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name = "Nicolas Cage"
+
+var raiting = 5.0
+raiting * 2
+
+
+
+
---
+++ Day1/Strings.playground/Contents.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+// MARK : Strings
+
+
+let actor = "Denzel Washington"
+let fileName = "paris.jpg"
+let result = "You Win!"
+
+let quote = " Then he tapped a sign saying \"believe\" and walked. away."
+
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let namelength = actor.count
+print(namelength)
+print(result.uppercased())
+
+
+print(movie.hasPrefix("A day"))
+
+print(fileName.hasSuffix(".jpg"))
