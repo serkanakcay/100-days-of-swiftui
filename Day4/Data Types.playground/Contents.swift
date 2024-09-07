315fbe47309aa2d8750af29d01b319fc78b29495
Serkan Akçay <serkanakcayyy@gmail.com>
2024-09-07 20:48:19 +0300

    Day4

---
+++ Day4/Data Types.playground/Contents.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+//MARK : Data Types
+
+let surname = "Lasso"
+
+var score = 0
+
+let surnameOne  :String = "Lasso"
+var scoreOne : Int = 0
+
+let playerName: String = "Roy"
+let luckyNumber: Int = 13
+
+let pi: Double = 3.141
+
+var isAuthenticated: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+
+var books: Set<String> = Set([
+"The Bluest Eye",
+"Foundation",
+"Girl, Womani other"
+])
+
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style: UIStyle = UIStyle.light
+
+style = .dark
+
+
+let userName: String
+// lots of complex logic
+
+userName = "@twotraws"
+//lots more complex logic
+
+print(userName)
+
+
+
+
+
